2963a2065f835a91d291256a0aa5230f8b6e2393
Matt Ellis <mellis@ncees.org>
2014-10-27 09:14:11 -0400

    Changed login from md5 to bcrypt

--- process_login.php
+++ process_login.php
@@ -3,20 +3,31 @@
 include('includes/config.php');
 
 $username       = $_POST['username'];
-$hashedPassword = md5($_POST['password']);
+//$hashedPassword = md5($_POST['password']);
+
+$options = array('cost' => 11);
+$hashedPassword = password_hash($_POST['password'], PASSWORD_BCRYPT, $options);
 
 /* check the database for correct password */
-$sql = "SELECT * FROM login WHERE username = '$username' AND password = '$hashedPassword'";
+//$sql = "SELECT * FROM login WHERE username = '$username' AND password = '$hashedPassword'";
+$sql = "SELECT * FROM login WHERE username = '$username'";
 $checkLogin = mysqli_query($con, $sql);
 
 if (mysqli_num_rows($checkLogin)) {
-    /* login user */
-    session_start();
-    $_SESSION['username'] = $username;
-
-    /* redirect to index */
-    header("location:index.php");
+    foreach ($checkLogin as $check) {
+        if (password_verify($_POST['password'], $check['password'])) {
+            /* login user */
+            session_start();
+            $_SESSION['username'] = $username;
+            
+            /* redirect to index */
+            header("location:index.php");
+        } else {
+            /* redirect back to login with an error */
+            header("location:login.php?error=1");
+        }
+    }
 } else {
-	/* redirect back to login with an error */
+    /* redirect back to login with an error */
     header("location:login.php?error=1");
 }
